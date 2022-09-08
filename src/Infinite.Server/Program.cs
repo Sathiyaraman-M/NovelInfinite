c7b302a381f980e75c6179d1817018dbef2c4d1d
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-08 20:29:30 +0530

    Added Microsoft.AspNetCore.Components.WebAssembly.Server to Infinite.Server.csproj and updated Program.cs

--- src/Infinite.Server/Program.cs
+++ src/Infinite.Server/Program.cs
@@ -1,6 +1,38 @@
+using Infinite.Core.Extensions;
+using Infinite.Server.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddCurrentUserService();
+builder.Services.AddDatabase(builder.Configuration, "DefaultConnection");
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapRazorPages();
+    endpoints.MapFallbackToFile("index.html");
+});
 
 app.Run();
