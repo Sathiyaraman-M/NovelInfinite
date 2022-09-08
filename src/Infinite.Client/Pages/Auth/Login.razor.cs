74652bfb76c6b6828a7622dc32919462ac03ac66
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-08 20:25:16 +0530

    Added Login.razor and updated App.razor

---
+++ src/Infinite.Client/Pages/Auth/Login.razor.cs
@@ -0,0 +1,55 @@
+﻿using Infinite.Shared.Requests.Identity;
+using MudBlazor;
+
+namespace Infinite.Client.Pages.Auth;
+
+public partial class Login
+{
+    protected override async Task OnInitializedAsync()
+    {
+        var user = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User;
+        if (user.Identity?.IsAuthenticated ?? false)
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+
+    private readonly TokenRequest _loginModel = new TokenRequest();
+
+    private static void SubmitAsync()
+    {
+        // var result = await loginHttpClient.Login(_loginModel);
+        // if (result.Succeeded)
+        // {
+        //     Snackbar.Add(result.Messages[0], Severity.Success);
+        //     NavigationManager.NavigateTo("/");
+        // }
+        // else
+        // {
+        //     foreach (var message in result.Messages)
+        //     {
+        //         Snackbar.Add(message, Severity.Error);
+        //     }
+        // }
+    }
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    private void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
