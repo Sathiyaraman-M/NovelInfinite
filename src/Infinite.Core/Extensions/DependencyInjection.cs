6931b2b15f7bebdd311b9b1bcd9dd4e50fe8c766
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-08 20:27:45 +0530

    Added DependencyInjection.cs in Infinite.Core project

---
+++ src/Infinite.Core/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Infinite.Core.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infinite.Core.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration, string connectionName)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString(connectionName)));
+        return services;
+    }
+}
