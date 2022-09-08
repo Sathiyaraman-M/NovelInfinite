b56b44bd32ae4167f6113798ceafdd57832d0370
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-08 20:23:20 +0530

    Added PermissionConstants.cs

---
+++ src/Infinite.Shared/Constants/PermissionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Infinite.Shared.Constants;
+
+public static class ApplicationClaimTypes
+{
+    public const string Permission = "Permission";
+}
+
+public static class Permissions
+{
+    
+}
