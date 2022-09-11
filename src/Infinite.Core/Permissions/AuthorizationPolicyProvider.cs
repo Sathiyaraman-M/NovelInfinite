a26293dbb51169e82fb38aed47bb0bd0f2c583bb
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 10:37:18 +0530

    Added AuthorizationPolicyProvider.cs

---
+++ src/Infinite.Core/Permissions/AuthorizationPolicyProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace Infinite.Core.Permissions;
+
+public class AuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+{
+    private readonly AuthorizationOptions _options;
+    public AuthorizationPolicyProvider(IOptions<AuthorizationOptions> options) : base(options)
+    {
+        _options = options.Value;
+    }
+
+    public override async Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+    {
+        return await base.GetPolicyAsync(policyName) ??
+               new AuthorizationPolicyBuilder()
+                   .AddRequirements(new PermissionRequirement(policyName))
+                   .Build();
+    }
+}
