623cea4fc40606723b0ba4661b439ca55cfa4cb1
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 10:35:27 +0530

    Added RoleConstants.cs

---
+++ src/Infinite.Shared/Constants/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Infinite.Shared.Constants;
+
+public static class RoleConstants
+{
+    public const string Admin = "Admin";
+    public const string Faculty = "Faculty";
+    public const string Student = "Student";
+}
