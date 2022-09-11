fa1075b689673234a7a3e8ac7c0b1492100a1443
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 10:35:56 +0530

    Added PermissionRequirement.cs

---
+++ src/Infinite.Core/Permissions/PermissionRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Infinite.Core.Permissions;
+
+public class PermissionRequirement : IAuthorizationRequirement
+{
+    public string PermissionName { get; }
+
+    public PermissionRequirement(string permissionName)
+    {
+        PermissionName = permissionName;
+    }
+}
