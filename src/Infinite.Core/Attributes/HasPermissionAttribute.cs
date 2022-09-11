85327e7fa4a627db30eb15a0774fb7f7e9bd51f4
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 10:44:41 +0530

    Added HasPermissionAttribute.cs

---
+++ src/Infinite.Core/Attributes/HasPermissionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Infinite.Shared.Constants;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Infinite.Core.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+public class HasPermissionAttribute : AuthorizeAttribute
+{
+    public HasPermissionAttribute(AppPermissions appPermissions) : base(appPermissions.ToString())
+    {
+        
+    }
+}
