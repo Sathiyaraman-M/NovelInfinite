e0860e5e98e4052975f65931e8e431207e7999da
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 17:55:06 +0530

    Updated RoleConstants.cs

--- src/Infinite.Shared/Constants/RoleConstants.cs
+++ src/Infinite.Shared/Constants/RoleConstants.cs
@@ -2,7 +2,6 @@
 
 public static class RoleConstants
 {
-    public const string Admin = "Admin";
-    public const string Faculty = "Faculty";
-    public const string Student = "Student";
+    public const string Internal = "Internal";
+    public const string General = "General";
 }
