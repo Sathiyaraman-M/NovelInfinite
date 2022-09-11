53c51553f0d06b11e1619d22047ba64bbc0bbeb3
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 10:34:07 +0530

    Updated AppUser.cs

--- src/Infinite.Shared/Entities/AppUser.cs
+++ src/Infinite.Shared/Entities/AppUser.cs
@@ -1,8 +1,25 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Infinite.Shared.Contracts;
+using Microsoft.AspNetCore.Identity;
 
 namespace Infinite.Shared.Entities;
 
-public class AppUser : IdentityUser
+public class AppUser : IdentityUser, IAuditableEntity<string>
 {
+    public string FullName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string ProfilePictureDataUrl { get; set; }
+    public bool IsActive { get; set; }
+
+    public string CreatedBy { get; set; }
+    public string CreatedByUserId { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string LastModifiedBy { get; set; }
+    public string LastModifiedByUserId { get; set; }
+    public DateTime? LastModifiedOn { get; set; }
+    public bool IsDeleted { get; set; }
     
+    public string RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
 }
