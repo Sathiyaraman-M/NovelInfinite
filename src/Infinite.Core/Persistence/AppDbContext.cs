44daaf05189bb0747bfbec4f771e0ba8a9822a52
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 10:58:13 +0530

    Added OnModelCreating override to AppDbContext.cs

--- src/Infinite.Core/Persistence/AppDbContext.cs
+++ src/Infinite.Core/Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Infinite.Shared.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +10,17 @@ public class AppDbContext : IdentityDbContext<AppUser>
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<AppUser>().ToTable("AppUsers", "dbo");
+        builder.Entity<IdentityRole>().ToTable("AppRoles", "dbo");
+        builder.Entity<IdentityUserRole<string>>().ToTable("AppUserRoles", "dbo");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("AppUserClaims", "dbo");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("AppRoleClaims", "dbo");
+        builder.Ignore<IdentityUserLogin<string>>();
+        builder.Ignore<IdentityUserToken<string>>();
     }
 }
