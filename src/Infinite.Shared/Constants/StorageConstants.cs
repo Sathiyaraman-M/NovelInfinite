44ddda9dc2cba63b3992366f8b85384b1e678484
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-11 17:55:19 +0530

    Updated StorageConstants.cs

--- src/Infinite.Shared/Constants/StorageConstants.cs
+++ src/Infinite.Shared/Constants/StorageConstants.cs
@@ -5,6 +5,7 @@ public static class StorageConstants
     public const string ClientPreference = "clientPreference";
 
     public const string AuthToken = "authToken";
+    public const string InternalAuthToken = "internalAuthToken";
     public const string RefreshToken = "refreshToken";
     public const string UserImageUrl = "userImageURL";
 }
