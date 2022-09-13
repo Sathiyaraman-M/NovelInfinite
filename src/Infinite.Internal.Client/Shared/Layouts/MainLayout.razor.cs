ee62c4159c2b2a3201609654fefb6a075f26c80b
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-13 21:42:20 +0530

    Added a Navigation Drawer in MainLayout.razor in Internal App

--- src/Infinite.Internal.Client/Shared/Layouts/MainLayout.razor.cs
+++ src/Infinite.Internal.Client/Shared/Layouts/MainLayout.razor.cs
@@ -1,13 +1,31 @@
-﻿namespace Infinite.Internal.Client.Shared.Layouts;
+﻿using System.Security.Claims;
+using Infinite.Internal.Client.Extensions;
+
+namespace Infinite.Internal.Client.Shared.Layouts;
 
 public partial class MainLayout
 {
+    private bool _open;
+    private ClaimsPrincipal _user;
+    private string _userName = "";
+    private string _fullName = "";
+    private string _firstLetterOfName;
+    protected override async Task OnInitializedAsync()
+    {
+        var state = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        _user = state.User;
+        _userName = _user.GetUserName();
+        _fullName = _user.GetFullName();
+        _firstLetterOfName = _userName[0].ToString();
+        await base.OnInitializedAsync();
+    }
+
     private async Task Logout()
     {
         if(await DialogService.ShowMessageBox("Log out?", "Are you sure want to logout?", "Yes", "No") == true)
         {
             await AuthenticationHttpClient.Logout();
-            NavigationManager.NavigateTo("/");
+            NavigationManager.NavigateTo("/", true);
         }
     }
 }
