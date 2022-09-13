d30752c78e135d6f818fa4128a06481e20d6f3ed
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-13 21:53:49 +0530

    Added additional request/response model classes

---
+++ src/Infinite.Shared/Requests/Identity/ToggleUserStatusRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Infinite.Shared.Requests.Identity;
+
+public class ToggleUserStatusRequest
+{
+    public bool ActivateUser { get; set; }
+    public string UserId { get; set; }
+}
---
+++ src/Infinite.Shared/Requests/Identity/UpdateUserRolesRequest.cs
@@ -0,0 +1,9 @@
+﻿using Infinite.Shared.Responses.Identity;
+
+namespace Infinite.Shared.Requests.Identity;
+
+public class UpdateUserRolesRequest
+{
+    public string UserId { get; set; }
+    public IList<UserRoleModel> UserRoles { get; set; }
+}
