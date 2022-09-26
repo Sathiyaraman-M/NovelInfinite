1c7b1214e7b4befa2d20a11e6c642e22da94786e
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-26 14:57:35 +0530

    Added ManageController.cs

---
+++ src/Infinite.Server/Controllers/ManageController.cs
@@ -0,0 +1,28 @@
+﻿using Infinite.Client.Extensions;
+using Infinite.Core.Features;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infinite.Server.Controllers;
+
+[Authorize]
+[Route("api/manage")]
+[ApiController]
+public class ManageController : ControllerBase
+{
+    private readonly IManageAccountService _manageAccountService;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public ManageController(IManageAccountService manageAccountService, IHttpContextAccessor httpContextAccessor)
+    {
+        _manageAccountService = manageAccountService;
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
+    [HttpGet("portfolio")]
+    public async Task<IActionResult> GetCurrentUserPortFolio()
+    {
+        var userId = _httpContextAccessor.HttpContext?.User.GetUserId();
+        return Ok(await _manageAccountService.GetPortFolioMd(userId));
+    }
+}
