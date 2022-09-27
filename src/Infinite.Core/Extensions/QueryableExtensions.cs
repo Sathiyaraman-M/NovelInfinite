bf6ede60ab323c042dcb28a69317740770edca56
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-09-27 21:14:25 +0530

    Added QueryableExtensions.cs

---
+++ src/Infinite.Core/Extensions/QueryableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Infinite.Shared.Wrapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infinite.Core.Extensions;
+
+public static class QueryableExtensions
+{
+    public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize) where T : class
+    {
+        if (source == null) throw new Exception("IQueryable source for pagination is empty.");
+        pageNumber = pageNumber == 0 ? 1 : pageNumber;
+        pageSize = pageSize == 0 ? 10 : pageSize;
+        var count = await source.CountAsync();
+        pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return PaginatedResult<T>.Success(items, count, pageNumber, pageSize);
+    }
+}
