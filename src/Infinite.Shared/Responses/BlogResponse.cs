99d69d594a857bc74cfe8a824401c04c368cbf69
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-10-06 20:43:28 +0530

    Added BlogResponse.cs

---
+++ src/Infinite.Shared/Responses/BlogResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Infinite.Shared.Responses;
+
+public class BlogResponse
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string AuthorName { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastEditedDate { get; set; }
+    public Visibility Visibility { get; set; }
+}
