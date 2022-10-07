e9af411bb68acdf55ce00b65e413fd4427e40cb6
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-10-07 18:02:51 +0530

    Added PersonalBlogsSearchFilterSpecification.cs

---
+++ src/Infinite.Core/Specifications/PersonalBlogsSearchFilterSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Infinite.Core.Specifications.Base;
+using Infinite.Shared.Entities;
+
+namespace Infinite.Core.Specifications;
+
+public class PersonalBlogsSearchFilterSpecification : BaseSpecification<Blog>
+{
+    public PersonalBlogsSearchFilterSpecification(string searchString)
+    {
+        if (string.IsNullOrEmpty(searchString))
+        {
+            FilterCondition = p => true;
+        }
+        else
+        {
+            searchString = searchString.ToLower();
+            FilterCondition = p => p.Title.ToLower().Contains(searchString);
+        }
+    }
+}
