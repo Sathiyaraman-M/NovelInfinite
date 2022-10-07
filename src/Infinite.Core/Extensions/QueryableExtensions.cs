b5e0a0cccd77eb587486ef324db4d4e88fb748e2
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-10-07 18:02:04 +0530

    Added SpecificationExtensions and included method in QueryableExtensions

--- src/Infinite.Core/Extensions/QueryableExtensions.cs
+++ src/Infinite.Core/Extensions/QueryableExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq.Expressions;
+using Infinite.Core.Specifications.Base;
+using Infinite.Shared.Contracts;
 using Infinite.Shared.Wrapper;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +17,21 @@ public static class QueryableExtensions
         pageNumber = pageNumber <= 0 ? 1 : pageNumber;
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
         return PaginatedResult<T>.Success(items, count, pageNumber, pageSize);
+    }
+
+    public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> spec) where T : class, IEntity
+    {
+        if (spec == null) 
+            return query;
+        
+        if (spec.FilterCondition != null)
+            return query.Where(spec.FilterCondition);
+
+        query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+        query = spec.IncludeStrings.Aggregate(query, (current, include) => current.Include(include));
+
+        return query;
     }
 
     public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> query, bool condition, Expression<Func<TSource, bool>> predicate)
---
+++ src/Infinite.Core/Extensions/SpecificationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Infinite.Core.Specifications.Base;
+using Infinite.Shared.Contracts;
+
+namespace Infinite.Core.Extensions;
+
+public static class SpecificationExtensions
+{
+    public static ISpecification<T> And<T>(this ISpecification<T> left, ISpecification<T> right) where T : class, IEntity
+    {
+        return new AndSpecification<T>(left, right);
+    }
+
+    public static ISpecification<T> Or<T>(this ISpecification<T> left, ISpecification<T> right) where T : class, IEntity
+    {
+        return new OrSpecification<T>(left, right);
+    }
+}
