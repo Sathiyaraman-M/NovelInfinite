3773fdeabedae88311f363814c1eff3fa7570c4a
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-10-10 18:32:15 +0530

    Added BlogDraftResponse

---
+++ src/Infinite.Shared/Responses/BlogDraftResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Infinite.Shared.Responses;
+
+public class BlogDraftResponse
+{
+    public string Id { get; set; }
+    public string BlogId { get; set; } = Guid.Empty.ToString();
+    public DateTime SaveDateTime { get; set; }
+    public string Title { get; set; }
+    public string AuthorName { get; set; }
+}
